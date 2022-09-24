9ea5f8b3bdd682d4f249e34d4d457afa6baf9c1d
Naveen.Pranesh <praneshnaveen@gmail.com>
2022-09-24 21:48:40 +0100

    initial commit

---
+++ JavaScript/src/refactor-me/ProductDataConsolidator.js
@@ -0,0 +1,11 @@
+﻿function ProductDataConsolidator() { }
+
+ProductDataConsolidator.get = function (a) {
+	var l = new LawnmowerRepository().getAll();
+	var p = new PhoneCaseRepository().getAll();
+	var t = new TShirtRepository().getAll();
+	var products = [];
+	var data = [l,p,t]
+	{data.map((x,index) => { x.map(val => products.push({id:val.id,name:val.name,price:(val.price * a).toFixed(2),type: index===0?"Lawnmower":index ===1 ?"Phone Case":"T-Shirt" }))})}
+	return products;
+}
---
+++ JavaScript/src/refactor-me/ProductDataRenderer.js
@@ -0,0 +1,34 @@
+﻿function ProductDataRenderer() { }
+
+ProductDataRenderer.prototype.render = function () {
+	var data = [{id:"nzdProducts",code:"(NZD)",price:1},{id:"usdProducts",code:"(USD)",price:0.76},	{id:"euProducts",code:"(euro)",price:0.67}]
+	data.map((val=>{
+		var a = loaddata(val.price,val.code) 
+		document.getElementById(val.id).innerHTML = a
+	}))
+ }
+function loaddata(price,code) {
+	var data = '<table class="table table-striped">'
+	+'	<thead>'
+	+'		<tr><td colspan="3">Products'
+	+'  ' + code
+	+'</td></tr>'
+	+'		<tr>'
+	+'			<td>Name</td>'
+	+'			<td>Price</td>'
+	+'			<td>Type</td>'
+	+'		</tr>'
+	+'	</thead>'
+	+ '	<tbody>';
+	
+var n = ProductDataConsolidator.get(price);
+for (var i = 0; i < n.length; i++) {
+	data +='<tr>'
+	+		'<td>' + n[i].name +'</td>'
+	+		'<td>' + n[i].price + '</td>'
+	+		'<td>' + n[i].type + '</td>'
+	+	'</tr>';
+}
+data += '</tbody></table>';
+return data;
+}
